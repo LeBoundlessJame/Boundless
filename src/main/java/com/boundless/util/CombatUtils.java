3f8deeee621a7539c65d3679f8b3446d66979aee
LeBoundlessJame <LeBoundlessJame@users.noreply.github.com>
2025-07-28 15:36:55 +0100

    Big progress on combat visuals

--- src/main/java/com/boundless/util/CombatUtils.java
+++ src/main/java/com/boundless/util/CombatUtils.java
@@ -1,5 +1,6 @@
 package com.boundless.util;
 
+import com.boundless.BoundlessAPI;
 import com.boundless.ability.combat.AttackDataBuilder;
 import com.boundless.entity.hero_action.HeroActionEntity;
 import com.boundless.registry.DataComponentRegistry;
@@ -7,6 +8,8 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.sound.SoundCategory;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.Vec3d;
 
 import static com.boundless.registry.DataComponentRegistry.ATTACK_END;
 
@@ -45,8 +48,16 @@ public class CombatUtils {
     public static void basicAttackLogic(AttackDataBuilder attack, LivingEntity target) {
         DamageSource source = attack.getDamageSource();
         if (source == null) source = target.getDamageSources().generic();
+        PlayerEntity attacker = attack.getPlayer();
+
+        Vec3d attackerRotation = attacker.getRotationVector();
+        Vec3d effectPos = target.getPos().add(attackerRotation.normalize().multiply(-target.getWidth()).x, target.getHeight() / 2f, attackerRotation.normalize().multiply(-target.getWidth()).z);
+        Vec3d effectScale =  new Vec3d(target.getScale() * 0.5f, target.getScale() * 0.5f, target.getScale() * 0.5f);
+        Vec3d effectRotation = new Vec3d(attacker.getPitch(), attacker.getYaw() * -1, 0);
+
+        EffekUtils.playRotatedEffect(BoundlessAPI.identifier("melee_impact"), attacker, effectPos, effectScale, effectRotation);
         target.damage(source, attack.getDamage());
-        target.takeKnockback(attack.getKnockbackStrength(), attack.getPlayer().getRotationVector().x * -1, attack.getPlayer().getRotationVector().z * -1);
+        target.takeKnockback(attack.getKnockbackStrength(), attackerRotation.x * -1, attackerRotation.z * -1);
         target.velocityModified = true;
     }
 
